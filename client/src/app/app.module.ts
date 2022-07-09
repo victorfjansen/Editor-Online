832f15a8875baed03a6ca6218f0a02b6b765261e
Victor Jansen <victorfjansen@gmail.com>
2022-07-08 23:09:07 -0300

    adding head component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './helpers/container/container.component';
+import { HeaderComponent } from './template/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContainerComponent
+    ContainerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
