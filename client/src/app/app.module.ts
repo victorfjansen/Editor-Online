265f10830405b5781810f30481504adce0986d01
Victor Jansen <victorfjansen@gmail.com>
2022-07-08 23:27:57 -0300

    adjusting shared modules for better organization

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { HelpersModule } from './helpers/helpers.module'
-import { TemplateModule } from './template/template.module'
+import { HelpersModule } from './shared/helpers/helpers.module'
+import { TemplateModule } from './shared/template/template.module'
 
 @NgModule({
   declarations: [AppComponent],
